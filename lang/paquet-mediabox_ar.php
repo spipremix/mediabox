2f716e31bdc8db74bbac241f9b1bb09b9613f120
eric <eric@smellup.net>
2011-07-30 14:52:13 +0000

    Début des tests du nouveau fichier paquet.xml. On ajoute pour chaque plugin du core : - le paquet.xml sous le nom _paquet.xml pour éviter une utilisation par défaut à la place de plugin.xml Il suffira de le renommer pour effectuer les tests. - les fichiers de langue nécessaire à la description et au slogan. - un fichier _paquet-migration qui contient les commandes svn pour migrer ou ajouter des fichiers. Ce fichier n'a plus d'autre intérêt que de lister les fichiers à renommer pour retrouver le fonctionnement des balises disparues : install, fonctions et options.

---
+++ lang/paquet-mediabox_ar.php
@@ -0,0 +1,25 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-mediabox
+// Langue: ar
+// Date: 30-07-2011 15:01:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mediabox_description' => 'افتراضياً، يتم إضفاء صندوق الفرجة على كل الوصلات الى الصور (لها نوع خاصية يصف mime/type الصورة) إضافة الى الوصلات المزودة بنمط mediabox.
+
+من الممكن إعداد كل وصلة على حدة من خلال أنماط إضافية:
+-* يتيح <code>boxIframe</code>  فتح الوصلة في إطار iframe
+-* يتيج <code>boxWidth-350px</code> تحديد عرض ٣٥٠ نقطة للإطار
+-* يتيح <code>boxHeight-90pc</code> تحديد ارتفاع ٩٠٪ للإطار
+
+وتتيح لوحة إعداد تعديل الإعدادات العامة حسب الرغبة كما تتيح اختيار شكل الصندوق من بين الأشكال المتاحة',
+	'mediabox_slogan' => 'صندوق الفرجة',
+);
+?>
---
+++ lang/paquet-mediabox_de.php
@@ -0,0 +1,17 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-mediabox
+// Langue: de
+// Date: 30-07-2011 15:01:37
+// Items: 1
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mediabox_slogan' => 'Multimedia-Box',
+);
+?>
