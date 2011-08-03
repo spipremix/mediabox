2c317779f974c024b8ef97194999c433989e7c16
eric <eric@smellup.net>
2011-08-03 21:01:52 +0000

    Ajout des traductions existantes pour les noms des plugins du core (5 plugins sur 24)

--- lang/paquet-mediabox_ar.php
+++ lang/paquet-mediabox_ar.php
@@ -4,8 +4,8 @@
 // Fichier produit par PlugOnet
 // Module: paquet-mediabox
 // Langue: ar
-// Date: 30-07-2011 15:01:37
-// Items: 2
+// Date: 03-08-2011 19:45:21
+// Items: 3
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
@@ -14,12 +14,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 // M
 	'mediabox_description' => 'افتراضياً، يتم إضفاء صندوق الفرجة على كل الوصلات الى الصور (لها نوع خاصية يصف mime/type الصورة) إضافة الى الوصلات المزودة بنمط mediabox.
 
+
+
 من الممكن إعداد كل وصلة على حدة من خلال أنماط إضافية:
+
 -* يتيح <code>boxIframe</code>  فتح الوصلة في إطار iframe
+
 -* يتيج <code>boxWidth-350px</code> تحديد عرض ٣٥٠ نقطة للإطار
+
 -* يتيح <code>boxHeight-90pc</code> تحديد ارتفاع ٩٠٪ للإطار
 
+
+
 وتتيح لوحة إعداد تعديل الإعدادات العامة حسب الرغبة كما تتيح اختيار شكل الصندوق من بين الأشكال المتاحة',
+	'mediabox_nom' => 'صندوق الفرجة',
 	'mediabox_slogan' => 'صندوق الفرجة',
 );
 ?>
--- lang/paquet-mediabox_de.php
+++ lang/paquet-mediabox_de.php
@@ -4,14 +4,15 @@
 // Fichier produit par PlugOnet
 // Module: paquet-mediabox
 // Langue: de
-// Date: 30-07-2011 15:01:37
-// Items: 1
+// Date: 03-08-2011 19:45:21
+// Items: 2
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 // M
+	'mediabox_nom' => 'MediaBox',
 	'mediabox_slogan' => 'Multimedia-Box',
 );
 ?>
