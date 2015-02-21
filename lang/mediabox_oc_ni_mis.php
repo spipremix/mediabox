31e7e85ddb7a6cac6b2241910b00aef47a6bf0ce
sylvain <sylvain@oliver-fr.org>
2015-02-21 04:33:13 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ mediabox] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/mediabox_oc_ni_mis.php
@@ -0,0 +1,43 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mediabox?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'bouton_reinitialiser' => 'Reinitialisà',
+	'boxstr_close' => 'Fermà',
+	'boxstr_current' => '@current@/@total@',
+	'boxstr_next' => 'Seguent',
+	'boxstr_previous' => 'Prechedent',
+	'boxstr_slideshowStart' => 'Presentacioun',
+	'boxstr_slideshowStop' => 'Arrestà',
+	'boxstr_zoom' => 'Zoom',
+
+	// E
+	'explication_selecteur' => 'Endicàs la bressai dei elemen que desgancheran la bouòta. (Espressioun CSS o destenduda jQuery)',
+	'explication_selecteur_galerie' => 'Endicàs la bressai dei elemen da groupà en galerìa. (Espressioun CSS o destenduda jQuery)',
+
+	// L
+	'label_active' => 'Ativà la bouòta Multimedia soubre lou sit public',
+	'label_apparence' => 'Aparença',
+	'label_choix_transition_elastic' => 'Elàstica',
+	'label_choix_transition_fade' => 'Founduda encadenada',
+	'label_choix_transition_none' => 'Sensa efet de transicioun',
+	'label_selecteur_commun' => 'En general',
+	'label_selecteur_galerie' => 'En galerìa',
+	'label_speed' => 'Vitessa de transicioun (ms)',
+	'label_splash' => 'Splash Bouòta',
+	'label_splash_url' => 'URL a afichà',
+	'label_traiter_toutes_images' => 'Image',
+	'label_transition' => 'Transicioun tra doui afichage',
+
+	// T
+	'titre_menu_box' => 'Bouòta Multimedia',
+	'titre_page_configurer_box' => 'Counfiguracioun de la Bouòta multimedia'
+);
+
+?>
