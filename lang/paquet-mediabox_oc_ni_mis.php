9a600072bd8d2fd2a3d01cdcfb1a99ea46d3f6dd
sylvain <sylvain@oliver-fr.org>
2015-02-24 04:33:23 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ paquet-mediabox] Export depuis http://trad.spip.net de la langue oc_ni_mis

--- lang/paquet-mediabox_oc_ni_mis.php
+++ lang/paquet-mediabox_oc_ni_mis.php
@@ -8,6 +8,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
+	'mediabox_description' => 'En mancança, toui lu estac devers li image (embé un atribut type descrivent lou mime/type de l’image) couma pura lu estac embé la classa <code>.mediabox</code> soun enriquit per la bouòta multimedia.
+
+	Es poussible de counfigurà cada estac au cas per cas embé dei classa suplementari :
+
+-* <code>boxIframe</code> permet de demandà da durbì l’estac en una bouòta iframe ;
+
+-* <code>boxWidth-350px</code> permet de spechificà una larguessa de 350px per la bouòta ;
+
+-* <code>boxHeight-90pc</code> permet de spechificà una autessa de 90% per la bouòta.
+
+	Un panèu de counfiguracioun vous permet de moudificà lu reglage general a la vouòstra counveniença, couma pura l’aspet de la bouòta tra lu abilhage dispounible.
+
+	Aqueu plugin founciouna sus lu esquèletrou qu’an una balisa <code>#INSERT_HEAD</code>',
 	'mediabox_nom' => 'MediaBox',
 	'mediabox_slogan' => 'Bouòta multimedia'
 );
