5855e7cbfe2c82f3b010babc6ea84ab7616639d6
rporto <rporto@calepino.com>
2015-04-21 03:33:32 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ mediabox] Export depuis http://trad.spip.net de la langue pt_br

--- lang/mediabox_pt_br.php
+++ lang/mediabox_pt_br.php
@@ -13,41 +13,41 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'boxstr_current' => '@current@/@total@',
 	'boxstr_next' => 'Próxima',
 	'boxstr_previous' => 'Anterior',
-	'boxstr_slideshowStart' => 'Diaporama',
+	'boxstr_slideshowStart' => 'Slideshow',
 	'boxstr_slideshowStop' => 'Parar',
 	'boxstr_zoom' => 'Zoom',
 
 	// E
-	'explication_selecteur' => 'Indique os elementos-alvo que irão acionar a caixa. (Expressão CSS ou estendido jQuery)',
-	'explication_selecteur_galerie' => 'Indique os elementos-alvo a serem agrupados na galeria. (Expressão CSS ou estendido jQuery)',
-	'explication_splash_url' => 'Indique o URL do arquivo a ser exibido em uma caixa na primeira consulta no site público.',
-	'explication_traiter_toutes_images' => 'Inserir uma caixa em todas as imagens?',
+	'explication_selecteur' => 'Indique os elementos-alvo que irão acionar o box. (Expressão CSS ou âmbito jQuery)',
+	'explication_selecteur_galerie' => 'Indique os elementos-alvo a serem agrupados na galeria. (Expressão CSS ou âmbito jQuery)',
+	'explication_splash_url' => 'Indique o URL do arquivo a ser exibido em um box no primeiro acesso no site público.',
+	'explication_traiter_toutes_images' => 'Inserir um box em todas as imagens?',
 
 	// L
-	'label_active' => 'Ativar a Caixa Multimedia no site público',
-	'label_apparence' => 'Aparência da câmera',
+	'label_active' => 'Ativar o box multimídia no site público',
+	'label_apparence' => 'Aparência',
 	'label_aucun_style' => 'Não insirir nenhum tema padrão',
 	'label_choix_transition_elastic' => 'Elástico',
 	'label_choix_transition_fade' => 'Dissolvido',
 	'label_choix_transition_none' => 'Sem efeito de transição',
-	'label_maxheight' => 'Altura Maxi (% ou px)',
-	'label_maxwidth' => 'Largura Maxi (% ou px)',
-	'label_minheight' => 'Altura Mini (% ou px)',
-	'label_minwidth' => 'Largura Mini (% ou px)',
+	'label_maxheight' => 'Altura máxima (% ou px)',
+	'label_maxwidth' => 'Largura máxima (% ou px)',
+	'label_minheight' => 'Altura mínima (% ou px)',
+	'label_minwidth' => 'Largura mínima (% ou px)',
 	'label_opacite' => 'Opacidade do fundo',
 	'label_selecteur_commun' => 'Em geral',
 	'label_selecteur_galerie' => 'Na galeria',
-	'label_skin' => 'Aparência dos botões',
-	'label_slideshow_speed' => 'Tempo de exposição das fotos do diaporama (ms)',
+	'label_skin' => 'Tema visual',
+	'label_slideshow_speed' => 'Tempo de exibição das fotos do slideshow (ms)',
 	'label_speed' => 'Velocidade da transição (ms)',
-	'label_splash' => 'Caixa Splash',
-	'label_splash_url' => 'URL a divulgar',
+	'label_splash' => 'Splash Box',
+	'label_splash_url' => 'URL a ser exibida',
 	'label_traiter_toutes_images' => 'Imagens',
-	'label_transition' => 'Transição entre telas',
+	'label_transition' => 'Transição entre exibições',
 
 	// T
-	'titre_menu_box' => 'Caixa Multimedia',
-	'titre_page_configurer_box' => 'Configuração da Caixa Multimedia'
+	'titre_menu_box' => 'Box Multimídia',
+	'titre_page_configurer_box' => 'Configuração do Box Multimídia'
 );
 
 ?>
