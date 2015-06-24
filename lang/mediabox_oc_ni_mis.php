fcee6f4395d1473629684d086550bf34cee0b36f
spip <spip@oliver-fr.org>
2015-06-24 03:32:32 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ mediabox] Export depuis http://trad.spip.net de la langue oc_ni_mis
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/mediabox@90469 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/mediabox_oc_ni_mis.php
+++ lang/mediabox_oc_ni_mis.php
@@ -20,15 +20,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// E
 	'explication_selecteur' => 'Endicàs la bressai dei elemen que desgancheran la bouòta. (Espressioun CSS o destenduda jQuery)',
 	'explication_selecteur_galerie' => 'Endicàs la bressai dei elemen da groupà en galerìa. (Espressioun CSS o destenduda jQuery)',
-	'explication_splash_url' => 'Endicàs l’url dóu media a afichà automaticamen en una bouòta per la premièra visita sus lou sit public.',
+	'explication_splash_url' => 'Endicàs l’url dóu media da afichà automaticamen en una bouòta per la premièra visita sus lou sit pùblicou.',
 	'explication_traiter_toutes_images' => 'Inserì una bouòta soubre touti li image ?',
 
 	// L
-	'label_active' => 'Ativà la bouòta Multimedia soubre lou sit public',
+	'label_active' => 'Ativà la bouòta Multimedia soubre lou sit pùblicou',
 	'label_apparence' => 'Aparença',
 	'label_aucun_style' => 'Inserì mìnga ablhage en mancança',
 	'label_choix_transition_elastic' => 'Elàstica',
-	'label_choix_transition_fade' => 'Founduda encadenada',
+	'label_choix_transition_fade' => 'Foundut encadenat',
 	'label_choix_transition_none' => 'Sensa efet de transicioun',
 	'label_maxheight' => 'Autessa Massimala (% o px)',
 	'label_maxwidth' => 'Larguessa Massimala  (% o px)',
@@ -41,7 +41,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_slideshow_speed' => 'Tems d’espousicioun dei fotò de la presentacioun (ms)',
 	'label_speed' => 'Vitessa de transicioun (ms)',
 	'label_splash' => 'Splash Bouòta',
-	'label_splash_url' => 'URL a afichà',
+	'label_splash_url' => 'URL da afichà',
 	'label_traiter_toutes_images' => 'Image',
 	'label_transition' => 'Transicioun tra doui afichage',
 
