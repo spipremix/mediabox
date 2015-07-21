d871ef127be12bb949947c6d8830071989e2ef96
real3t <real3t@gmail.com>
2015-07-21 03:34:44 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ mediabox] Export depuis http://trad.spip.net de la langue pt

--- lang/mediabox_pt.php
+++ lang/mediabox_pt.php
@@ -41,7 +41,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_slideshow_speed' => 'Tempo de exibição das fotos do slideshow (ms)',
 	'label_speed' => 'Velocidade de transição (ms)',
 	'label_splash' => 'Caixa Splash ',
+	'label_splash_height' => 'Altura (% ou px)',
 	'label_splash_url' => 'URL a exibir',
+	'label_splash_width' => 'Largura (% ou px)',
 	'label_traiter_toutes_images' => 'Imagens',
 	'label_transition' => 'Transição entre duas visualizações',
 
