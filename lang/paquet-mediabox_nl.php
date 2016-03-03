40b1ad212f00e217dbc2057d363ccc8b5d34c750
spip <spip@allesfrans.com>
2016-03-03 04:35:20 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ paquet-mediabox] Export depuis http://trad.spip.net de la langue nl
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/mediabox@95720 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- lang/paquet-mediabox_nl.php
+++ lang/paquet-mediabox_nl.php
@@ -10,9 +10,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mediabox_description' => 'Standaard worden alle links naar beelden (met een type attribuut beschrijving van de beeld mime/type) en de links met een class <code>.mediabox </ code> verrijkt door de mediabox.
+	'mediabox_description' => 'Standaard worden alle links naar afbeeldingen (met een type attribuut dat de mime/type beschrijft) en de links met een class <code>.mediabox </code> verrijkt door de mediabox.
 
-Het is mogelijk om elke link telkens met extra klassen te configureren :
+Het is mogelijk om elke link telkens met extra classes te configureren:
 
 - * <code>boxIframe</code> om de link in een iframe doos te tonen;
 
@@ -20,9 +20,9 @@ Het is mogelijk om elke link telkens met extra klassen te configureren :
 
 - <code>boxHeight-90pc</code>  om een hoogte van 90% voor de box te specificeren.
 
-In een configuratiepaneel kunt u de algemene instellingen voor uw gemak bewerken, en ook het uiterlijk van de doos vervangen uit de beschikbare skins.
+In een configuratiepaneel kun je de algemene instellingen bewerken, alsmede het uiterlijk van de doos aanpassen met de beschikbare skins.
 
-Deze plugin werkt op de skeletten die de <code>#INSERT_HEAD</ code> SPIP tag gebruiken.',
+Deze plugin werkt op de skeletten die het SPIP baken <code>#INSERT_HEAD</ code> gebruiken.',
 	'mediabox_nom' => 'MediaBox',
 	'mediabox_slogan' => 'Multimedia Box'
 );
