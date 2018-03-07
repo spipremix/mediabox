4cc8c446deffcf98d73c26e6e01e82bae3bb24b2
gpl <gpl@long-player.net>
2018-03-07 04:36:38 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ paquet-mediabox] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-mediabox_eo.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-mediabox?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediabox_description' => 'Defaŭlte, ĉiujn ligilojn al bildoj (kun atributo „type“, kiu difinas la bildan MIME-tipon) kaj la ligilojn kun la klaso <code>.mediabox</code> pliriĉigas la plurmedia skatolo.
+ 
+	Eblas agordi ĉiun ligilon unuope per pliaj klasoj :
+	
+-* <code>boxIframe</code>, por ke la ligilo malfermiĝu en IFRAME-skatolo ;
+
+-* <code>boxWidth-350px</code> por skatolo largâ je 350px (px = bilderoj) ;
+
+-* <code>boxHeight-90pc</code> por skatolo alta je 90%.
+
+	Agordilaro ebligas vin modifi plurajn aspektojn laŭvole.
+	
+	Tiu kromprogramo funkcias ĉe la skeletoj, kiuj uzas la markon <code>#INSERT_HEAD</code>.',
+	'mediabox_nom' => 'MediaBox',
+	'mediabox_slogan' => 'Plurmedia skatolo'
+);
