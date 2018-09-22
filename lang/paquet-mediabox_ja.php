784b2aaa91a81022791dda072dcc3002b9f6b2c4
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-22 03:38:09 +0000

    [Salvatore] [source:_core_/plugins/mediabox/lang/ paquet-mediabox] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/paquet-mediabox_ja.php
@@ -0,0 +1,28 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-mediabox?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mediabox_description' => 'デフォルトでは、<code>.mediabox </code>クラスとのリンクだけでなく、画像へのすべてのリンクは（MIME/イメージタイプ）、マルチメディアボックスによって取り上げられます。
+
+追加のクラスを使用して、各リンクをケースバイケースで設定することができます。
+
+ -* <code>boxIframe</code>はiframe内のボックスでリンクを開くように頼むことができます。
+
+ -* <code>boxWidth-350px</code>でボックスの幅を350pxに指定できます。
+
+ -* <code>boxHeight-90pc</code>では、ボックスの高さを90％に指定できます。
+
+コントロールパネルでは、便利なときに一般設定を変更できるほか、使用可能なスキンの中のボックスの外観も変更できます。
+
+このプラグインは、<code>#INSERT_HEAD</code>タグを持つレイアウト上で動作します。',
+	'mediabox_nom' => 'メディアボックス',
+	'mediabox_slogan' => 'マルチメディアボックス'
+);
